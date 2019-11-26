e06e5fc3557b8b4a5a033e5f3b34851f84622e90
rachman-karim <raka_rpl@yahoo.co.id>
2019-11-26 11:38:35 +0700

    upload data project pertama

---
+++ main/tes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Rachman")
+	fmt.Println(len("It's works!"))
+	fmt.Println("It's works!")
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("1.2 + 1.3 =", 1.2+1.3)
+
+	err := errors.New("Data not complete!")
+	err = nil
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("raka")
+	}
+}
