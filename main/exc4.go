325028ce0045cbff95d5ae98e20a215c280cd6c3
rachman-karim <raka_rpl@yahoo.co.id>
2019-12-10 21:52:23 +0700

    Tambah os.args 3 data baru

--- main/exc4.go
+++ main/exc4.go
@@ -16,8 +16,9 @@ func main() {
 	var argsRaw = os.Args
 	//raka.Printf("-> %#v\n", argsRaw)
 
-	P := person{"Rachman Karim", "jalan jalan", "prigrammer", "Cari sesuatu yang baru"}
-	P2 := person{"Riska Aprian", "jalan pondok gede", "prigrammer", "Cari sesuatu yang baru"}
+	P := person{"Rachman Karim", "jalan jalan", "programmer", "Cari sesuatu yang baru"}
+	P2 := person{"Riska Aprian", "jalan pondok gede", "programmer", "Cari sesuatu yang baru"}
+	P3 := person{"Tarnoto", "jalan ujung", "programmer", "Cari sesuatu yang baru"}
 	//raka.Printf("hasil", argsRaw)
 	//configPath := os.Args
 
@@ -33,6 +34,12 @@ func main() {
 		raka.Println("Alamat saya di", P2.alamat)
 		raka.Println("Pekerjaan saya adalah", P2.pekerjaan)
 		raka.Println("Alasan memilih kelas golang", P2.pekerjaan)
+	} else if argsRaw[1] == "3" {
+		//configpath = os.Args[2]
+		raka.Println("Nama saya adalah", P3.name)
+		raka.Println("Alamat saya di", P3.alamat)
+		raka.Println("Pekerjaan saya adalah", P3.pekerjaan)
+		raka.Println("Alasan memilih kelas golang", P3.pekerjaan)
 	}
 
 }
